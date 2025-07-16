bd0d7840c36b2d442ea1762c36ba690ecfebe77f
Agung Hutri <agung@qtasmin.com>
2025-07-16 20:08:12 +0700

    tes

--- app/lib/getBaseUrl.ts
+++ app/lib/getBaseUrl.ts
@@ -1,5 +1,5 @@
 // lib/getBaseUrl.js
-export function getBaseUrl() {
+export default function getBaseUrl() {
   if (typeof window !== 'undefined') return ''; // client-side
   return process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'; // server-side
 }
